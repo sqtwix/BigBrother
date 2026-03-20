dc24f3a384bafc6bdecc4d1206feb253cb6d871b
sqtwix <ivan20140767@gmail.com>
2026-03-20 14:04:29 +0300

    feat(Application): ITrackerService

---
+++ BigBrotherApp/BigBrother.Application/Interfaces/ITrackerService.cs
@@ -0,0 +1,17 @@
+﻿using BigBrother.Domain.Entities;
+
+namespace BigBrother.Application.Interfaces;
+
+public interface ITrackerService
+{
+    // Inteface for methods of TrackerService
+
+    Task StartTrackingAsync();
+    Task StopTrackingAsync();
+    void UpdateCurrentSession(object state);
+    Task CloseCurrentSessionAsync();
+    Task<List<ActivitySession>> GetSessionsForPeriodAsync(DateTime start, DateTime end);
+    Task<TimeSpan> GetTotalActiveTimeForDateAsync(DateTime date);
+    Task<List<(string ProcessName, TimeSpan TotalTime)>> GetTopProcessesAsync(DateTime start, DateTime end, int top = 5)
+}
+
--- BigBrotherApp/BigBrother.Infrustructure/Repositories/ActivitySessionRepository.cs
+++ BigBrotherApp/BigBrother.Infrustructure/Repositories/ActivitySessionRepository.cs
@@ -62,4 +62,3 @@ public class ActivitySessionRepository : IActivitySessionRepository
         await _context.SaveChangesAsync();
     }
 }
-
