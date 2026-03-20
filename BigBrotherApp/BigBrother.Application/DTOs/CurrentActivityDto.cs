ad0f13b9b3e0bf598413d3087c9961bf93311996
sqtwix <ivan20140767@gmail.com>
2026-03-20 16:24:58 +0300

    feat(Application): new methods in ITrackerService and DTO

---
+++ BigBrotherApp/BigBrother.Application/DTOs/CurrentActivityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BigBrother.Application.DTOs;
+
+public class CurrentActivityDto
+{
+    // Dto for info about current activity
+
+    public string ProcessName { get; set; } = string.Empty;
+    public string WindowTitle { get; set; } = string.Empty;
+    public TimeSpan Duration { get; set; }
+}
+
--- BigBrotherApp/BigBrother.Application/Interfaces/ITrackerService.cs
+++ BigBrotherApp/BigBrother.Application/Interfaces/ITrackerService.cs
@@ -1,6 +1,8 @@
 ﻿using BigBrother.Domain.Entities;
+using BigBrother.Application.DTOs;
 
 namespace BigBrother.Application.Interfaces;
+
 
 public interface ITrackerService
 {
@@ -10,8 +12,15 @@ public interface ITrackerService
     Task StopTrackingAsync();
     void UpdateCurrentSession(object state);
     Task CloseCurrentSessionAsync();
+
+    Task<CurrentActivityDto> GetCurrentActivityAsync(); 
+
+    Task<TimeSpan> GetSystemUptimeAsync();
+
     Task<List<ActivitySession>> GetSessionsForPeriodAsync(DateTime start, DateTime end);
     Task<TimeSpan> GetTotalActiveTimeForDateAsync(DateTime date);
-    Task<List<(string ProcessName, TimeSpan TotalTime)>> GetTopProcessesAsync(DateTime start, DateTime end, int top = 5)
+    Task<List<(string ProcessName, TimeSpan TotalTime)>> GetTopProcessesAsync(DateTime start, DateTime end, int top = 5);
+
+    Task<List<ActivitySession>> GetSessionsSinceSystemStartAsync();
 }
 
